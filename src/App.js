7fbf46973d547c9d479888f060c3233e8b928111
Elias Flores <eliasflores13@outlook.com>
2018-10-10 12:24:33 -0400

    Added Redux to the aplicaction. Now it works with redux and one state

--- src/App.js
+++ src/App.js
@@ -1,69 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+import store from './store';
+import { getRandomQuote } from './actions';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: '',
-      quote: {},
-    }
-  }
 
-  componentDidMount() {
-    this.changeColor();
-    this.getRandomQuote();
-  }
-
-  getRandomQuote = () => {
-    axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-      .then((response) => {
-        const quotes = response.data.quotes;
-        const randomIndex = Math.round(Math.random() * (quotes.length - 1));
-        this.setState({
-          quote: quotes[randomIndex]
-        });
-      })
-      .catch((error) => {
-        console.log('An error has occurred: ' + error.message);
-      });
-  };
-
-  changeColor = () => {
-    const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-    const newColor = colors[Math.round(Math.random() * (colors.length - 1))];
-    this.setState({
-      color: newColor,
-    });
-  }
-
-  handleClick = (event) => {
-    this.changeColor();
-    this.getRandomQuote();
+  handleClick = () => {
+    store.dispatch(getRandomQuote());
   };
 
   render() {
-    document.body.style.backgroundColor = this.state.color;
+    document.body.style.backgroundColor = store.getState().color;
 
     const colorStyle = {
-      color: this.state.color,
+      color: store.getState().color,
     };
 
     const buttonsColor = {
-      backgroundColor: this.state.color,
+      backgroundColor: store.getState().color,
     };
 
-    const tweetURL = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${this.state.quote.quote}" - ${this.state.quote.author}`;
+    const tweetURL = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${store.getState().quote.quote}" - ${store.getState().quote.author}`;
 
     return ( 
       <div className="App">
         <div id='quoteBox'>
           <h2 style={colorStyle} id='quoteText'>
             <i className="fas fa-quote-left"></i>
-            {this.state.quote.quote}
+            {store.getState().quote.quote}
           </h2>
-          <span style={colorStyle} className='quoteAuthor'>- {this.state.quote.author}</span>
+          <span style={colorStyle} className='quoteAuthor'>- {store.getState().quote.author}</span>
           <div id='buttons'>
             <a 
             style={buttonsColor} 
